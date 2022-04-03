23f257795a2d15611d6f625f5eac90ee93992ef0
ligaopeng123 <1170234932@qq.com>
2022-04-03 09:29:07 +0800

    useDebounce useThrottle

--- packages/useDebounce/src/useDebounce.tsx
+++ packages/useDebounce/src/useDebounce.tsx
@@ -3,6 +3,7 @@ import { useCallback, useRef, useEffect } from "react";
 type Fn = (...props: any) => any;
 type Delay = number;
 type Dep = any[];
+
 export const useDebounce = (fn: Fn, delay: Delay = 200, dep: Dep = []) => {
     const {current} = useRef<any>({fn, timer: null});
     useEffect(function () {
