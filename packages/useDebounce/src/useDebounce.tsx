4eb3be7052fb1837a1cb238a9a36a72a775ac8aa
ligaopeng123 <1170234932@qq.com>
2022-04-06 11:55:06 +0800

    useDebounce 空包问题

--- packages/useDebounce/src/useDebounce.tsx
+++ packages/useDebounce/src/useDebounce.tsx
@@ -4,7 +4,7 @@ import type {DebounceOptions} from "@gaopeng123/utils.function";
 
 type Fn = (...props: any) => any;
 type Dep = any[];
-
+// 去抖 借助useRef
 export const useDebounce = (fn: Fn, wait = 200, options: DebounceOptions = {}, dep: Dep = []) => {
     const {current} = useRef<any>({fn, timer: null});
     useEffect(function () {
