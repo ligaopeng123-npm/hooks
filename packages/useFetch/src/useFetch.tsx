b48cdfa4939404e9aad876024c22a0341db2ab2e
ligaopeng123 <gaopeng@123>
2022-06-09 21:35:45 +0800

    合入问题：useFetch 支持传入依赖 合入人: pgli 合入时间：2022年6月9日21:35:21

--- packages/useFetch/src/useFetch.tsx
+++ packages/useFetch/src/useFetch.tsx
@@ -12,10 +12,11 @@
 import React, {useState, useEffect} from 'react';
 import {createFetch, MethodEnum} from "@gaopeng123/fetch";
 import {Options} from "@gaopeng123/fetch/dist/typing";
-import {isFunction} from "@gaopeng123/utils.types";
+import {isArray, isFunction} from "@gaopeng123/utils.types";
 
 export type CheckResponse = (props: any) => any;
-const useFetch = (url: string, options: Options, checkResponse?: CheckResponse) => {
+export type Deps = Array<any>
+const useFetch = (url: string, options: Options, checkResponse?: CheckResponse, deps?: Deps) => {
     const [data, setData] = useState<any>();
     const [error, setError] = useState<string | Error>('');
     const [loading, setLoading] = useState<boolean>(true);
@@ -36,33 +37,33 @@ const useFetch = (url: string, options: Options, checkResponse?: CheckResponse) 
         return () => {
             abortController.abort();
         }
-    }, []);
+    }, isArray(deps) ? deps : []);
     return [loading, error, data];
 };
 
 export default useFetch;
 
-export const useGet = (url: string, options?: Options, checkResponse?: CheckResponse) => {
-    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.get}, options), checkResponse);
+export const useGet = (url: string, options?: Options, checkResponse?: CheckResponse, deps?: Deps) => {
+    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.get}, options), checkResponse, deps);
     return [loading, error, data];
 }
 
-export const usePost = (url: string, options?: Options, checkResponse?: CheckResponse) => {
-    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.post}, options), checkResponse);
+export const usePost = (url: string, options?: Options, checkResponse?: CheckResponse, deps?: Deps) => {
+    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.post}, options), checkResponse, deps);
     return [loading, error, data];
 }
 
-export const usePut = (url: string, options?: Options, checkResponse?: CheckResponse) => {
-    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.put}, options), checkResponse);
+export const usePut = (url: string, options?: Options, checkResponse?: CheckResponse, deps?: Deps) => {
+    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.put}, options), checkResponse, deps);
     return [loading, error, data];
 }
 
-export const useDel = (url: string, options?: Options, checkResponse?: CheckResponse) => {
-    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.del}, options), checkResponse);
+export const useDel = (url: string, options?: Options, checkResponse?: CheckResponse, deps?: Deps) => {
+    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.del}, options), checkResponse, deps);
     return [loading, error, data];
 }
 
-export const usePatch = (url: string, options?: Options, checkResponse?: CheckResponse) => {
-    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.patch}, options), checkResponse);
+export const usePatch = (url: string, options?: Options, checkResponse?: CheckResponse, deps?: Deps) => {
+    const [loading, error, data] = useFetch(url, Object.assign({method: MethodEnum.patch}, options), checkResponse, deps);
     return [loading, error, data];
 }
