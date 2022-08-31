767606002b8962499fc30af63d6a4febbf43e033
ligaopeng123 <gaopeng@123>
2022-08-31 09:52:00 +0800

    合入问题：useZoom合入 合入人: pgli 合入时间: 2022年8月31日09:51:56

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -9,21 +9,23 @@
  * @版权所有: pgli
  *
  ********************************************************************* */
-export {useClock} from "@gaopeng123/hooks.use-clock";
-export type {ClockDate} from "@gaopeng123/hooks.use-clock";
-export {useEasing, easingFuncs} from "@gaopeng123/hooks.use-easing";
-export type {EasingType, UseEasingProps, Easing} from "@gaopeng123/hooks.use-easing";
-export {useFetch, useGet, usePost, useDel, usePut, usePatch} from "@gaopeng123/hooks.use-fetch";
-export {useMergeValue} from "@gaopeng123/hooks.use-merge-value";
-export {usePoller} from "@gaopeng123/hooks.use-poller";
-export type {PollerMark, PollerProps} from "@gaopeng123/hooks.use-poller";
-export {useResize} from "@gaopeng123/hooks.use-resize";
-export type {ResizeProps} from "@gaopeng123/hooks.use-resize";
-export {useScale} from "@gaopeng123/hooks.use-scale";
-export type {ScaleProps} from "@gaopeng123/hooks.use-scale";
-export {useWorker} from "@gaopeng123/hooks.use-worker";
-export type {UseWorkerProps} from "@gaopeng123/hooks.use-worker";
-export {useDebounce} from "@gaopeng123/hooks.use-debounce";
-export type {DebounceOptions} from "@gaopeng123/hooks.use-debounce";
-export {useThrottle} from "@gaopeng123/hooks.use-throttle";
-export type {ThrottleOptions} from "@gaopeng123/hooks.use-throttle";
+export { useClock } from "@gaopeng123/hooks.use-clock";
+export type { ClockDate } from "@gaopeng123/hooks.use-clock";
+export { useEasing, easingFuncs } from "@gaopeng123/hooks.use-easing";
+export type { EasingType, UseEasingProps, Easing } from "@gaopeng123/hooks.use-easing";
+export { useFetch, useGet, usePost, useDel, usePut, usePatch } from "@gaopeng123/hooks.use-fetch";
+export { useMergeValue } from "@gaopeng123/hooks.use-merge-value";
+export { usePoller } from "@gaopeng123/hooks.use-poller";
+export type { PollerMark, PollerProps } from "@gaopeng123/hooks.use-poller";
+export { useResize } from "@gaopeng123/hooks.use-resize";
+export type { ResizeProps } from "@gaopeng123/hooks.use-resize";
+export { useScale } from "@gaopeng123/hooks.use-scale";
+export type { ScaleProps } from "@gaopeng123/hooks.use-scale";
+export { useZoom } from "@gaopeng123/hooks.use-zoom";
+export type { ZoomProps } from "@gaopeng123/hooks.use-zoom";
+export { useWorker } from "@gaopeng123/hooks.use-worker";
+export type { UseWorkerProps } from "@gaopeng123/hooks.use-worker";
+export { useDebounce } from "@gaopeng123/hooks.use-debounce";
+export type { DebounceOptions } from "@gaopeng123/hooks.use-debounce";
+export { useThrottle } from "@gaopeng123/hooks.use-throttle";
+export type { ThrottleOptions } from "@gaopeng123/hooks.use-throttle";
