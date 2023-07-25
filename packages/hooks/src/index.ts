e150ce9a7173d9072cbd7dc21c2a1d14039d718c
ligaopeng123 <1170234932@qq.com>
2023-07-25 09:36:32 +0800

    feat: initCreateFetch

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -9,26 +9,12 @@
  * @版权所有: pgli
  *
  ********************************************************************* */
+export { useClock } from "@gaopeng123/hooks.use-clock";
+export type { ClockDate } from "@gaopeng123/hooks.use-clock";
+export { useEasing, easingFuncs } from "@gaopeng123/hooks.use-easing";
+export type { EasingType, UseEasingProps, Easing } from "@gaopeng123/hooks.use-easing";
 export {
-    useClock
-}
-    from "@gaopeng123/hooks.use-clock";
-export type {
-    ClockDate
-}
-    from "@gaopeng123/hooks.use-clock";
-export {
-    useEasing,
-    easingFuncs
-}
-    from "@gaopeng123/hooks.use-easing";
-export type {
-    EasingType,
-    UseEasingProps,
-    Easing
-}
-    from "@gaopeng123/hooks.use-easing";
-export {
+    initCreateFetch,
     useFetch,
     useGet,
     usePost,
@@ -56,103 +42,29 @@ export {
     useCtrlPatch,
     useCtrlPut,
     useCtrlDel,
-}
-    from "@gaopeng123/hooks.use-fetch";
-export {
-    useMergeValue
-}
-    from "@gaopeng123/hooks.use-merge-value";
-export {
-    usePoller
-}
-    from "@gaopeng123/hooks.use-poller";
-export type {
-    PollerMark,
-    PollerProps
-}
-    from "@gaopeng123/hooks.use-poller";
-export {
-    useResize
-}
-    from "@gaopeng123/hooks.use-resize";
-export type {
-    ResizeProps
-}
-    from "@gaopeng123/hooks.use-resize";
-export {
-    useScale
-}
-    from "@gaopeng123/hooks.use-scale";
-export type {
-    ScaleProps
-}
-    from "@gaopeng123/hooks.use-scale";
-export {
-    useZoom
-}
-    from "@gaopeng123/hooks.use-zoom";
-export type {
-    ZoomProps
-}
-    from "@gaopeng123/hooks.use-zoom";
-export {
-    useWorker
-}
-    from "@gaopeng123/hooks.use-worker";
-export type {
-    UseWorkerProps
-}
-    from "@gaopeng123/hooks.use-worker";
-export {
-    useDebounce
-}
-    from "@gaopeng123/hooks.use-debounce";
-export type {
-    DebounceOptions
-}
-    from "@gaopeng123/hooks.use-debounce";
-export {
-    useThrottle
-}
-    from "@gaopeng123/hooks.use-throttle";
-export {
-    useThrottleAndMerge
-}
-    from "@gaopeng123/hooks.use-throttle-and-merge";
-export type {
-    ThrottleOptions
-}
-    from "@gaopeng123/hooks.use-throttle";
-export {
-    usePagination
-}
-    from "@gaopeng123/hooks.use-pagination";
-export type {
-    PaginationProps
-}
-    from "@gaopeng123/hooks.use-pagination";
-export {
-    useVisibility
-}
-    from "@gaopeng123/hooks.use-visibility";
-export type {
-    VisibilityProps
-}
-    from "@gaopeng123/hooks.use-visibility";
-export {
-    useBoolean
-}
-    from "@gaopeng123/hooks.use-boolean";
-export type {
-    BooleanBack
-}
-    from "@gaopeng123/hooks.use-boolean";
-export {
-    useReloadAfterStationary
-}
-    from "@gaopeng123/hooks.use-reload-after-stationary";
-export type {
-    OperateConfig
-}
-    from "@gaopeng123/hooks.use-reload-after-stationary"
+} from "@gaopeng123/hooks.use-fetch";
+export { useMergeValue } from "@gaopeng123/hooks.use-merge-value";
+export { usePoller } from "@gaopeng123/hooks.use-poller";
+export type { PollerMark, PollerProps } from "@gaopeng123/hooks.use-poller";
+export { useResize } from "@gaopeng123/hooks.use-resize";
+export type { ResizeProps } from "@gaopeng123/hooks.use-resize";
+export { useScale } from "@gaopeng123/hooks.use-scale";
+export type { ScaleProps } from "@gaopeng123/hooks.use-scale";
+export { useZoom } from "@gaopeng123/hooks.use-zoom";
+export type { ZoomProps } from "@gaopeng123/hooks.use-zoom";
+export { useWorker } from "@gaopeng123/hooks.use-worker";
+export type { UseWorkerProps } from "@gaopeng123/hooks.use-worker";
+export { useDebounce } from "@gaopeng123/hooks.use-debounce";
+export type { DebounceOptions } from "@gaopeng123/hooks.use-debounce";
+export { useThrottle } from "@gaopeng123/hooks.use-throttle";
+export { useThrottleAndMerge } from "@gaopeng123/hooks.use-throttle-and-merge";
+export type { ThrottleOptions } from "@gaopeng123/hooks.use-throttle";
+export { usePagination } from "@gaopeng123/hooks.use-pagination";
+export type { PaginationProps } from "@gaopeng123/hooks.use-pagination";
+export { useVisibility } from "@gaopeng123/hooks.use-visibility";
+export type { VisibilityProps } from "@gaopeng123/hooks.use-visibility";
+export { useBoolean } from "@gaopeng123/hooks.use-boolean";
+export type { BooleanBack } from "@gaopeng123/hooks.use-boolean";
+export { useReloadAfterStationary } from "@gaopeng123/hooks.use-reload-after-stationary";
+export type { OperateConfig } from "@gaopeng123/hooks.use-reload-after-stationary"
 
